e988079a72959e9c2732ae8b642109490eb4c23f
deshaun-byte <24AZUBUOGODe@hazelwick.org>
2026-09-25 13:57:07 +0000

    25/09/26

---
+++ task5/task23.py
@@ -0,0 +1,24 @@
+di=input("12/6=")
+if di == "2":
+	print("Correct")
+else:
+	print("Incorrect")
+mu=input("3*6=")
+if mu == "18":
+	print("you are correct")
+else:
+	print("Incorrect")
+ad=input("5+9=")
+if ad == "14":
+	print("you are correct")
+else:
+	print("Incorrect")
+su=input("10-3=")
+if su == "7":
+	print("you are correct")
+else:
+	print("Incorrect")
+if di == "2" and mu == "18" and ad == "14" and su == "7":
+	print("5/5")
+else:
+	print("you got some wrong answers")
